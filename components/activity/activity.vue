a93fb687e9d455b8f7396526d08bfc3e294908ee
naelmessaoudene <naelmessaoudene@gmail.com>
2021-04-20 22:44:58 +0200

    ✨ wip on activity view

---
+++ components/activity/activity.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="activity">
+    <h2 class="activity-title">S'enjailler</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, getModule, Vue} from 'nuxt-property-decorator'
+import SceneModule from "~/store/scene";
+
+@Component({})
+export default class activity extends Vue {
+
+  public mounted() {
+    console.log("eeeeeeee")
+  }
+
+
+}
+</script>
+
+<style lang="scss">
+.activity{
+  position: absolute;
+  z-index: 999;
+  width: 100vw;
+  height: 100vh;
+  background: white;
+  top: 0;
+  transform: translateY(100%);
+  &-title{
+    font-size: 40px;
+  }
+}
+</style>
--- pages/rooms/_roomName/_objectName/index.vue
+++ pages/rooms/_roomName/_objectName/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="page-container" data-namespace="rooms.roomName.objectName">
     <nuxt-link class="interactive-points" to="{{}}"></nuxt-link>
+    <div class="interactive-btn" >Découvrir le mot</div>
+    <activity></activity>
   </div>
 </template>
 
@@ -10,8 +12,14 @@ import {Context} from "@nuxt/types";
 import {RouteValidator} from "~/core/validators";
 import {URL_OBJECT_IDENTIFIER} from "~/core/enums";
 import {SceneManager} from "~/core/managers";
+import activity from "~/components/activity/activity.vue";
+import gsap from 'gsap'
 
-@Component
+@Component({
+  components:{
+    activity
+  }
+})
 export default class ObjectPage extends Vue {
 
   /**
@@ -25,10 +33,28 @@ export default class ObjectPage extends Vue {
     const objectIdentifier = <URL_OBJECT_IDENTIFIER>this.$route.params.objectName
     SceneManager.GLOBAL_SCENE.goToPresetPosition(objectIdentifier, 1, () => {
     })
+    let activity = document.querySelector('.interactive-btn')
+
+    activity?.addEventListener('click',()=>{
+
+      console.log("okokokok")
+      gsap.to('.activity',{translateY:0,duration:1})
+    })
   }
 }
 </script>
 
 <style lang="scss">
-
+.interactive-btn{
+  display: flex;
+  position: absolute;
+  z-index: 99;
+  top: 30%;
+  right: 20%;
+  text-decoration: none;
+  padding: 20px;
+  border-radius: 30px;
+  background: red;
+  color: white;
+}
 </style>
