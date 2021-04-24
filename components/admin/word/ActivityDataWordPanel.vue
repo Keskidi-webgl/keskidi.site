104020d84df34ce7531411e83e2978e1ca78d1b1
Théo <tochampion38@gmail.com>
2021-04-24 13:11:16 +0200

    ⚡ Imrpove performances on Activity data panel

--- components/admin/word/ActivityDataWordPanel.vue
+++ components/admin/word/ActivityDataWordPanel.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <b-overlay class="absolute-overlay" opacity="1" :show="onProgress" rounded="sm"></b-overlay>
     <!-- Modal container -->
     <div v-if="word" class="activity-data-modal-container">
       <!-- Create activity data modal -->
@@ -89,14 +90,17 @@
         <div v-if="word.activity_data" class="activity-data-infos-container">
           <b-card no-body>
             <b-tabs card>
-              <b-tab title="Objet 1" :class="{'bg-success': word.activity_data.object_one === word.activity_data.good_object }" active>
+              <b-tab title="Objet 1" active>
                 <b-card-text>{{word.activity_data.object_one}}</b-card-text>
               </b-tab>
-              <b-tab title="Objet 2" :class="{'success': word.activity_data.object_two === word.activity_data.good_object }">
+              <b-tab title="Objet 2">
                 <b-card-text>{{word.activity_data.object_two}}</b-card-text>
               </b-tab>
-              <b-tab title="Objet 3" :class="{'success': word.activity_data.object_three === word.activity_data.good_object }">
+              <b-tab title="Objet 3">
                 <b-card-text>{{word.activity_data.object_three}}</b-card-text>
+              </b-tab>
+              <b-tab title="Bon objet">
+                <b-card-text>{{word.activity_data.good_object}}</b-card-text>
               </b-tab>
             </b-tabs>
             <b-card-footer>
@@ -118,9 +122,11 @@
 import {Component, Prop, Vue} from 'nuxt-property-decorator'
 import {Word} from "~/core/types";
 import {ApiManager} from "~/core/managers";
+import AdminLayout from "~/layouts/admin.vue";
 
 @Component
 export default class ActivityDataWordPanel extends Vue {
+  public onProgress: boolean = false
   @Prop({type: Object, required: true}) readonly word!: Word
   public createActivityDataForm = {
     object_one: '',
@@ -136,35 +142,57 @@ export default class ActivityDataWordPanel extends Vue {
    * Call API to create Definition
    */
   async createActivityData() {
-    await ApiManager.request({
-      url: `/words/${this.word.id}/activityData`,
-      method: 'POST',
-      data: this.createActivityDataForm
-    })
-    this.$emit('reloadWordData')
+    this.onProgress = true
+    try {
+      const {data: createdActivityData} = await ApiManager.request({
+        url: `/words/${this.word.id}/activityData`,
+        method: 'POST',
+        data: this.createActivityDataForm
+      })
+      this.word.activity_data = createdActivityData
+      AdminLayout.successToast(this.$bvToast)
+    } catch (error) {
+      AdminLayout.errorToast(this.$bvToast)
+    }
+    this.onProgress = false
   }
 
   /**
    * Call API to update Definition
    */
   async updateActivityData() {
-    const {data: updateActivityData} = await ApiManager.request({
-      url: `/words/${this.word.id}/activityData`,
-      method: 'PUT',
-      data: this.word.activity_data
-    })
-    this.$emit('reloadWordData')
+    this.onProgress = true
+    try {
+      const {data: updateActivityData} = await ApiManager.request({
+        url: `/words/${this.word.id}/activityData`,
+        method: 'PUT',
+        data: this.word.activity_data
+      })
+      this.word.activity_data = updateActivityData
+      AdminLayout.successToast(this.$bvToast)
+    } catch (error) {
+      AdminLayout.errorToast(this.$bvToast)
+    }
+
+    this.onProgress = false
   }
 
   /**
    * Call API to delete Definition
    */
   async deleteActivityData() {
-    await ApiManager.request({
-      url: `/words/${this.word.id}/activityData`,
-      method: 'DELETE',
-    })
-    this.$emit('reloadWordData')
+    this.onProgress = true
+    try {
+      await ApiManager.request({
+        url: `/words/${this.word.id}/activityData`,
+        method: 'DELETE',
+      })
+      this.word.activity_data = null
+      AdminLayout.successToast(this.$bvToast)
+    } catch (error) {
+      AdminLayout.errorToast(this.$bvToast)
+    }
+    this.onProgress = false
   }
 
 }
