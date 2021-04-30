5cddf018c93359f04f753872f827d325e4dcc15b
Théo <tochampion38@gmail.com>
2021-04-30 12:24:12 +0200

    🔥 Clean activity code

---
+++ components/activities/ActivityOnboarding.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="activity-panel-onboarding">
+
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'nuxt-property-decorator'
+
+@Component({})
+
+export default class ActivityOnboarding extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+</style>
--- components/activities/ActivityPanel.vue
+++ components/activities/ActivityPanel.vue
@@ -1,25 +1,6 @@
 <template>
-  <div class="activity-container">
-    <!-- composant progress-bar    -->
-    <ActivityOne v-if="activityStore.currentActivity === 0"></ActivityOne>
-    <ActivityTwo v-if="activityStore.currentActivity === 1"></ActivityTwo>
-    <ActivityThree v-if="activityStore.currentActivity === 2"></ActivityThree>
-    <!--    overlay transition -->
-    <span class="leave-activity" @click="goToHome">
-      <img src="~/assets/img/cross.svg" alt="">
-    </span>
-    <!--  components  conclusion progression -->
+  <div class="activity-panel">
 
-    <!--
-    <section class="activity-page">
-      <aside class="activity-page-aside">
-
-      </aside>
-      <main class="activity-page-content">
-
-      </main>
-    </section>
-    -->
   </div>
 </template>
 
@@ -28,16 +9,8 @@ import {Component, getModule, Vue} from 'nuxt-property-decorator'
 import GlobalSceneStore from "~/store/globalScene"
 import {SceneManager} from "~/core/managers"
 import ActivityStore from "~/store/activity"
-import ActivityOne from "~/components/activities/activityOne/ActivityOne.vue"
-import ActivityTwo from "~/components/activities/activityTwo/ActivityTwo.vue"
-import ActivityThree from "~/components/activities/activityThree/ActivityThree.vue"
 
 @Component({
-  components: {
-    ActivityOne,
-    ActivityTwo,
-    ActivityThree
-  }
 })
 
 export default class ActivityPanel extends Vue {
@@ -57,77 +30,10 @@ export default class ActivityPanel extends Vue {
    * Callback when click to cancel cross
    */
   public destroyActivities() {
-    SceneManager.ACTIVITY_1_OBJECTS?.destroy()
-    SceneManager.ACTIVITY_1_TOM?.destroy()
-    SceneManager.ACTIVITY_2_OBJECTS?.destroy()
-    SceneManager.ACTIVITY_3_OBJECTS?.destroy()
-    SceneManager.ACTIVITY_3_TOM?.destroy()
-    this.activityStore.setCurrentActivity(null)
+
   }
 }
 </script>
 
 <style lang="scss">
-.activity-container {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 100;
-  background-color: #FFF8EE;
-  transform: translateY(100%);
-
-  .leave-activity {
-    width: 76px;
-    height: 76px;
-    position: fixed;
-    top: 20px;
-    right: 20px;
-    cursor: pointer;
-  }
-
-  .activity-page {
-    display: flex;
-    align-items: stretch;
-    height: 100%;
-
-    &-aside {
-      width: 30%;
-      max-width: 440px;
-      background: linear-gradient(107.28deg, #FF6644 29.48%, #FF9D6F 100%);
-      height: 100%;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-
-      .word-title {
-        font-size: $title-activity;
-        color: white;
-        text-align: center;
-        font-weight: bold;
-      }
-
-      .tom-canvas {
-        //width: 100% !important;
-        //height: 55% !important;
-        //max-height: 520px;
-        position: absolute;
-      }
-    }
-
-    &-content {
-      overflow-y: scroll;
-      flex: 1;
-    }
-  }
-}
-.common-text {
-  color: #000648;
-  font-size: 32px;
-  line-height: 48px;
-  font-weight: 500;
-}
 </style>
