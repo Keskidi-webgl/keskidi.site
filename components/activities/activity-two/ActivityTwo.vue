8cca67dafec6e15a166f11bece4019a139843d10
Théo <tochampion38@gmail.com>
2021-04-30 18:20:18 +0200

    ✨ Create Activity components

---
+++ components/activities/activity-two/ActivityTwo.vue
@@ -0,0 +1,45 @@
+<template>
+  <transition
+    v-on:enter="_onEnter"
+    v-on:leave="_onLeave">
+
+    <div class="activity-two">
+      <aside>
+
+      </aside>
+      <main></main>
+    </div>
+
+  </transition>
+</template>
+
+<script lang="ts">
+import {Component, getModule, Vue} from 'nuxt-property-decorator'
+import GlobalSceneStore from "~/store/globalScene"
+import ActivityStore from "~/store/activity"
+
+@Component({})
+export default class ActivityTwo extends Vue {
+  public globalSceneStore = getModule(GlobalSceneStore, this.$store)
+  public activityStore = getModule(ActivityStore, this.$store)
+
+
+  /**
+   * Callback when click to cancel cross
+   */
+  public destroyActivities() {
+
+  }
+
+  private _onEnter(el, done) {
+  }
+
+  private _onLeave(el) {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
