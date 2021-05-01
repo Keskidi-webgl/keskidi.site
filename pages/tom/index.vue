08cd849ba91b43bc9c36528dae30f8d0da5caf78
Amelie47 <ameliemouillacpro@gmail.com>
2021-05-01 18:39:12 +0200

    :lipstick: Add auth card style

--- pages/tom/index.vue
+++ pages/tom/index.vue
@@ -1,38 +1,103 @@
 <template>
   <div class="page-container" data-namespace="auth">
-
-    <CustomCard v-if="isReady && !this.authStore.isAuth" class="auth-container" background-color="white" width="610">
-      <h1 class="big-title main-font w-100">Wesh alors !</h1>
-      <p class="sub-title text-common secondary-font w-100">Bienvenue chez mes parents. Mi casa es tu Casa.</p>
-      <p class="w-100 main-font">Je me présente rapidement. Moi, c'est Tom, j'ai 18 ans ! J'adore jouer aux jeux vidéos, faire du skate et de la muscu.</p>
-      <p class="w-100 main-font">Je ne laisse pas n'importe qui rentrer chez moi. J'ai donc besoin que tu me donne ton email 😀</p>
-      <input type="text" v-model="dataFormAuth.email" class="form-control" id="email" placeholder="Ton email">
-      <div class="w-100 action-container">
-        <CustomButton @click.native="auth()" arrow-color="white" color="#000648" text="Valider"/>
+    <CustomCard
+      v-if="isReady && !displayFunfact"
+      class="auth-container"
+      background-color="white"
+      width="610"
+    >
+      <img class="doodle" src="~/assets/img/orange-doodles.svg" alt="" />
+      <div v-if="step == 1" class="w-100 step">
+        <h1 class="big-title main-font w-100 tom-title">Wesh alors !</h1>
+        <p class="sub-title text-common secondary-font w-100">
+          Bienvenue chez mes parents. Mi casa es tu Casa.
+        </p>
+        <p class="w-100 main-font">
+          Moi, c'est Tom, j'ai 18 ans !
+        </p>
+        <p class="w-100 main-font">
+          Tu trouveras différents mots que j'utilise au quotidien, dissimulés
+          dans différentes pièces de ma maison.
+        </p>
+        <div class="w-100 action-container">
+          <CustomButton
+            @click.native="next()"
+            arrow-color="white"
+            color="#000648"
+            text="Suivant"
+          />
+        </div>
       </div>
+      <div v-if="step == 2" class="w-100 step step-2">
+        <p class="w-100 main-font">
+          Pour continuer, j'ai besoin de ton <strong>adresse mail</strong> pour
+          que tu puisses garder ta <strong>progression</strong>.
+          <span>
+            <img
+              class="rgpd"
+              src="~/assets/img/info_RGPD.svg"
+              alt=""
+              @click="rgpd()"
+            />
+          </span>
+        </p>
+        <p class="w-100 main-font">
+          Et puis mes darons ne souhaitent pas laisser rentrer n'importe qui
+          chez nous...
+        </p>
+        <input
+          type="text"
+          v-model="dataFormAuth.email"
+          class="form-control"
+          id="email"
+          placeholder="Ton email"
+        />
+        <div class="w-100 action-container">
+          <CustomButton
+            @click.native="auth()"
+            arrow-color="white"
+            color="#000648"
+            text="Valider"
+          />
+        </div>
+      </div>
+
       <span class="leave-activity" @click="back()">
-        <img src="~/assets/img/cross.svg" alt="">
+        <img src="~/assets/img/cross.svg" alt="" />
       </span>
     </CustomCard>
 
-    <CustomCard v-if="isReady && this.authStore.isAuth" class="auth-container" background-color="white" width="610">
-      <p class="w-100">{{ currentFact.content }}</p>
-      <span class="leave-activity" @click="back()">
-        <img src="~/assets/img/cross.svg" alt="">
-      </span>
-      <div class="w-100 action-container">
-        <CustomButton @click.native="random()" arrow-color="white" color="#000648" text="Une autre"/>
+    <CustomCard
+      v-if="isReady && displayFunfact"
+      class="fun-container"
+      background-color="white"
+      width="610"
+    >
+      <img class="doodle" src="~/assets/img/orange-doodles.svg" alt="" />
+      <div class="w-100 step">
+        <h1 class="big-title main-font w-100 tom-title">Fun fact !</h1>
+        <p class="w-100">{{ currentFact.content }}</p>
+        <span class="leave-activity" @click="back()">
+          <img src="~/assets/img/cross.svg" alt="" />
+        </span>
+        <div class="w-100 action-container">
+          <CustomButton
+            @click.native="random()"
+            arrow-color="white"
+            color="#000648"
+            text="Une autre"
+          />
+        </div>
       </div>
     </CustomCard>
-
   </div>
 </template>
 
 <script lang="ts">
-import {Component, getModule, Vue} from 'nuxt-property-decorator';
-import {SceneManager} from "~/core/managers";
-import {AuthCredential, FunFactElement} from '~/core/types';
-import AuthStore from '~/store/auth';
+import { Component, getModule, Vue } from "nuxt-property-decorator";
+import { SceneManager } from "~/core/managers";
+import { AuthCredential, FunFactElement } from "~/core/types";
+import AuthStore from "~/store/auth";
 import CustomCard from "~/components/cards/CustomCard.vue";
 import CustomButton from "~/components/buttons/CustomButton.vue";
 
@@ -43,60 +108,96 @@ import CustomButton from "~/components/buttons/CustomButton.vue";
   }
 })
 export default class AuthPage extends Vue {
-  public onProgress: boolean = false
-  public autModule: AuthStore = getModule(AuthStore, this.$store)
+  public onProgress: boolean = false;
+  public autModule: AuthStore = getModule(AuthStore, this.$store);
   public dataFormAuth: AuthCredential = {
-    email: ''
-  }
-  public isReady: boolean = false
-  public authStore: AuthStore = getModule(AuthStore, this.$store)
+    email: ""
+  };
+  public isReady: boolean = false;
+  public authStore: AuthStore = getModule(AuthStore, this.$store);
   public currentFact: FunFactElement = {
-    content: ''
+    content: ""
+  };
+  public funfacts: Array<
+    FunFactElement
+  > = require("../../core/datas/funfacts.json");
+  public step: number = 1;
+  public displayFunfact: boolean = this.authStore.isAuth;
+
+  mounted() {
+    SceneManager.GLOBAL_SCENE.disableParallax();
+    SceneManager.GLOBAL_SCENE.goToPresetPosition("tom", 2, () => {
+      this.isReady = true;
+    });
+
+    this.random();
   }
-  public funfacts: Array<FunFactElement> = require('../../core/datas/funfacts.json')
-
-    mounted() {
-      SceneManager.GLOBAL_SCENE.disableParallax()
-      SceneManager.GLOBAL_SCENE.goToPresetPosition('tom', 2, () => {
-        this.isReady = true;
-      })
-
-      this.random()
-    }
 
   async auth() {
     if (this.dataFormAuth.email) {
       try {
-        await this.autModule.auth(this.dataFormAuth)
-        this.back()
+        await this.autModule.auth(this.dataFormAuth);
+        this.back();
       } catch (e) {
-        this.onProgress = false
+        this.onProgress = false;
       }
     }
   }
 
   beforeDestroy() {
-    SceneManager.GLOBAL_SCENE.enableParallax()
+    SceneManager.GLOBAL_SCENE.enableParallax();
   }
 
   back() {
-    this.$router.push('/');
+    this.$router.push("/");
   }
 
   random() {
-    this.currentFact = this.funfacts[Math.floor(Math.random() * this.funfacts.length)]
+    let current = this.currentFact;
+    let newFunFact = this.funfacts[
+      Math.floor(Math.random() * this.funfacts.length)
+    ];
+    if (newFunFact != current) this.currentFact = newFunFact;
+    else this.random();
+  }
+
+  next() {
+    this.step++;
+  }
+
+  rgpd() {
+    // TODO RGPD
+    alert("Lien vers rgpd");
   }
 }
 </script>
 
 <style scoped lang="scss">
-.auth-container {
+.auth-container,
+.fun-container {
   z-index: 20;
   position: absolute;
   left: 120px;
   top: 50%;
   transform: translateY(-50%);
   color: $dark-blue;
+
+  .step {
+    position: relative;
+    z-index: 5;
+
+    .tom-title {
+      margin-top: 120px;
+    }
+  }
+
+  // General
+  .doodle {
+    position: absolute;
+    top: 100px;
+    right: 50px;
+    z-index: 1;
+  }
 
   .leave-activity {
     width: 76px;
@@ -114,10 +215,21 @@ export default class AuthPage extends Vue {
 
   .action-container {
     padding-top: 24px;
+    display: flex;
+    justify-content: center;
   }
 
   .action {
     float: left;
+  }
+}
+.auth-container {
+  .step-2 {
+    margin-top: 160px;
+
+    .rgpd {
+      cursor: pointer;
+    }
   }
 }
 </style>
