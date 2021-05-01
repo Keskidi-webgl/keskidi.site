f2d1957d8e2778a0e07f8fa79fb8151b45f6ebc1
Amelie47 <ameliemouillacpro@gmail.com>
2021-05-01 20:28:32 +0200

    :lipstick: Update navigation panel buttons style - Add hover name rooms

--- core/config/global-scene/rooms/types.ts
+++ core/config/global-scene/rooms/types.ts
@@ -1,11 +1,12 @@
-import {ROOM_SLUG} from "~/core/config/global-scene/rooms/enums";
-import {RoomObject} from "~/core/config/global-scene/room-objects/types";
+import { ROOM_SLUG } from "~/core/config/global-scene/rooms/enums";
+import { RoomObject } from "~/core/config/global-scene/room-objects/types";
 
 export type Room = {
-  urlSlug: ROOM_SLUG
-  name: string
-  nextRoom: () => Room
-  previousRoom: () => Room
-  fullUrl: string
-  objects: () => Array<RoomObject>
-}
+  urlSlug: ROOM_SLUG;
+  name: string;
+  nameForHuman: string;
+  nextRoom: () => Room;
+  previousRoom: () => Room;
+  fullUrl: string;
+  objects: () => Array<RoomObject>;
+};
