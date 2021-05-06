a86dc7ad96c12c07790bc60452a53769629a0bd7
Théo <tochampion38@gmail.com>
2021-05-06 02:28:32 +0200

    ✨ Restore globalscene initializer

--- core/utils/initializers/GlobalSceneInitializer.ts
+++ core/utils/initializers/GlobalSceneInitializer.ts
@@ -41,12 +41,11 @@ export default class GlobalSceneInitializer extends Initializers<{ canvas: HTMLC
   init() {
     GlobalScene.setSceneContext(this._createSceneContext())
     this._addSceneElements()
-    //this._addLights(true)
-    //this._createCanvasBackground()
-    //this._createPlanesBackground()
+    this._addLights(true)
+    this._createCanvasBackground()
+    this._createPlanesBackground()
     this._registerPresetPositions()
     this._optimizeScene()
-    //this._prepareFloor()
     this._configGUI()
     console.log(GlobalScene.context)
 
