bd9f8f68a5b37b287d93f61644ca6f34834917a8
Théo <tochampion38@gmail.com>
2021-05-08 02:23:19 +0200

    💫 Add animations on phone

--- core/config/global-scene/room-objects/mezzanine/PhoneRoomObject.ts
+++ core/config/global-scene/room-objects/mezzanine/PhoneRoomObject.ts
@@ -3,6 +3,10 @@ import { ROOM_OBJECT_SLUG } from "~/core/config/global-scene/room-objects/enums"
 import { Mezzanine } from "~/core/config/global-scene/rooms";
 import { WORD_ID } from "~/core/enums/word";
 import { ROOM_SLUG } from "~/core/config/global-scene/rooms/enums";
+import {AssetsManager} from "~/core/managers";
+import {GLTF_ASSET} from "~/core/enums";
+import GlobalScene from "~/core/scene/GlobalScene";
+import {LoopOnce} from "three";
 
 const PhoneRoomObject: RoomObject = {
   name: "phone",
@@ -11,7 +15,15 @@ const PhoneRoomObject: RoomObject = {
   room: () => Mezzanine,
 
   fullUrl: `/rooms/${ROOM_SLUG.MEZZANINE}/${ROOM_OBJECT_SLUG.PHONE}`,
-  wordId: WORD_ID.GHOSTER
+  wordId: WORD_ID.GHOSTER,
+  animation: () => {
+    const gltfObject = AssetsManager.getGltf(GLTF_ASSET.PHONE).data!
+    const animationClip = GlobalScene.context.generateAnimationAction(gltfObject.animations[0], GLTF_ASSET.PHONE)
+    animationClip.clampWhenFinished = true
+    animationClip.setLoop(LoopOnce, 1)
+    animationClip.play()
+    animationClip.reset()
+  }
 };
 
 export default PhoneRoomObject;
